093d0f73d37366c589d212fc1d2fe492725480b3
hanahot <21103903@usc.edu.ph>
2022-10-15 21:46:07 +0800

    Added Another Practice

---
+++ 20221005 Activity/ErrorException.java
@@ -0,0 +1,8 @@
+public class ErrorException extends Exception{
+    
+    // public ErrorException{
+    // }
+    public static void errorMessage(String error){
+        System.out.println(" --> Age is invalid! <--");
+    }
+}
---
+++ 20221005 Activity/Record.java
@@ -0,0 +1,49 @@
+import java.text.*;
+
+public class Act3IndivClass<DataType> {
+
+ //instance variables 
+ private String name; 
+ private int accountNum;
+ private double balance;
+ 
+ //the Account constructor
+ public Act3IndivClass(String initName, int initId, double initBalance){
+  
+  name = initName;
+  accountNum = initId;
+  balance = initBalance;
+ }
+
+ //method to deposit a specified amount into the account 
+ public void deposit(double amount){
+  balance = balance + amount;
+ }
+
+ //method to withdraw a specified amount from the account
+ public void withdraw(double amount, double fee){
+  balance = balance - amount - fee;
+ }
+
+ //getter method to return balance
+ public double getBalance(){
+  return balance;
+ } 
+ 
+ //toString method that returns the accounts information
+ public String toString(){
+  String result = " ";
+  NumberFormat fmt = NumberFormat.getCurrencyInstance();
+  result = " Name: " + name + " Account Number: " + accountNum + " Balance: " + fmt.format(balance);
+  return result;
+  
+ }
+ public int getAccountNum() {
+           return accountNum;
+ }
+ public void setAccountNum(int accountNum) {
+           this.accountNum = accountNum;
+ }
+
+ 
+}
